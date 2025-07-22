75de1baf05a9d1c5870646e638b7a36a59c9eae3
Edson Vitor <edson.vitor42@gmail.com>
2025-07-22 17:42:08 -0300

    chore: test

--- src/mlflow_server/start_mlflow.sh
+++ src/mlflow_server/start_mlflow.sh
@@ -5,7 +5,7 @@ echo "$GOOGLE_CREDENTIALS_JSON" > ./gcp-storage-service-account.json
 ls -l "$GOOGLE_APPLICATION_CREDENTIALS"
 
 mlflow server \
-  --backend-store-uri sqlite:///$MLFLOW_DB_PATH \
+  --backend-store-uri $MLFLOW_DB_PATH \
   --default-artifact-root $MLFLOW_ARTIFACT_PATH \
   --host 0.0.0.0 \
   --port 5000
--- start_mlflow.sh
+++ start_mlflow.sh
@@ -10,7 +10,7 @@ mkdir -p $MLFLOW_ARTIFACT_PATH
 ls -l "$GOOGLE_APPLICATION_CREDENTIALS"
 
 mlflow server \
-  --backend-store-uri sqlite:///$MLFLOW_DB_PATH \
+  --backend-store-uri $MLFLOW_DB_PATH \
   --default-artifact-root $MLFLOW_ARTIFACT_PATH \
   --host 0.0.0.0 \
   --port 5000
