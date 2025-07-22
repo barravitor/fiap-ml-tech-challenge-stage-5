f5b19858b467eb698209302cd789457bd17818b0
Edson Vitor <edson.vitor42@gmail.com>
2025-07-22 17:09:23 -0300

    chore: teste

--- src/api/start_api.sh
+++ src/api/start_api.sh
@@ -4,6 +4,4 @@ echo "$GOOGLE_CREDENTIALS_JSON" > ./gcp-storage-service-account.json
 
 ls -l "$GOOGLE_APPLICATION_CREDENTIALS"
 
-export PYTHONPATH=$(pwd)/src
-
-newrelic-admin run-program uvicorn src.api.main:app --host 0.0.0.0 --port 8000 --workers 1
+newrelic-admin run-program uvicorn api.main:app --host 0.0.0.0 --port 8000 --workers 1
