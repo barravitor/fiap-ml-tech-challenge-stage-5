8960f911745c8bfe38ae33ebeb2f3b54726fa474
Edson Vitor <edson.vitor42@gmail.com>
2025-07-22 12:20:31 -0300

    chore: config mlflow build

---
+++ src/mlflow_server/start_mlflow.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "$GOOGLE_CREDENTIALS_JSON" > /app/gcp-storage-service-account.json
+
+ls -l "$GOOGLE_APPLICATION_CREDENTIALS"
+
+mlflow server \
+  --backend-store-uri sqlite:///$MLFLOW_DB_PATH \
+  --default-artifact-root $MLFLOW_ARTIFACT_PATH \
+  --host 0.0.0.0 \
+  --port 5000
