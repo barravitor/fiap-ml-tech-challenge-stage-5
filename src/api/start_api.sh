c87a473888dcbe977de1d6de437d553ad9f42cb4
Edson Vitor <edson.vitor42@gmail.com>
2025-07-22 16:47:13 -0300

    fix: error on build

--- src/api/start_api.sh
+++ src/api/start_api.sh
@@ -4,6 +4,6 @@ echo "$GOOGLE_CREDENTIALS_JSON" > ./gcp-storage-service-account.json
 
 ls -l "$GOOGLE_APPLICATION_CREDENTIALS"
 
-export PYTHONPATH=$(pwd)/
+export PYTHONPATH=$(pwd)/api
 
 newrelic-admin run-program uvicorn api.main:app --host 0.0.0.0 --port 8000 --workers 1
